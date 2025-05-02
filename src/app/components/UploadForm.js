f26703a0c1d3bad57675ca6f9ae6225a95b614a1
Razan Kurouni <razankr07@gmail.com>
2025-05-03 01:20:24 +0400

    fix: update CORS to allow vercel domains

--- src/app/components/UploadForm.js
+++ src/app/components/UploadForm.js
@@ -17,7 +17,7 @@ export default function UploadForm({ onExtractedData }) {
     try {
         
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/parse-pdf`, // ✅ استخدم env بدل الرابط الصريح
+        `https://smart-pdf-backend-1.onrender.com/parse-pdf`, // ✅ استخدم env بدل الرابط الصريح
         formData,
         {
           headers: {
